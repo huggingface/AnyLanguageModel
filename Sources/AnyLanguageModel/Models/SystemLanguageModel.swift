47adb55143fd63dc8d41df03f5f24499b23fb281
james-333i <james@333i.co>
2026-09-02 08:56:57 -0700

    Honor maximumResponseTokens in Foundation Models generation options

--- Sources/AnyLanguageModel/Models/SystemLanguageModel.swift
+++ Sources/AnyLanguageModel/Models/SystemLanguageModel.swift
@@ -405,8 +405,9 @@
                 options.temperature = temperature
             }
 
-            // Note: FoundationModels.GenerationOptions may not have all properties
-            // Only set those that are available
+            if let maximumResponseTokens = self.maximumResponseTokens {
+                options.maximumResponseTokens = maximumResponseTokens
+            }
 
             return options
         }
